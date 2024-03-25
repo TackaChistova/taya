c9ac41283c5d6b45ef276ff6718019246c6d5782
TackaChistova <159020125+TackaChistova@users.noreply.github.com>
2024-03-25 14:49:39 +0300

    Update 1112131415.cpp

--- 1112131415.cpp
+++ 1112131415.cpp
@@ -7,16 +7,19 @@ int itc_second_max_num(long long number){
 long long maxi = -10;
 long long maxi2 = -10;
 long long n = 0;
+if(number < 0){
+    number = number * -1;
+}
 if(number / 10 == 0){
     return -1;
 }
 else{
     while(number != 0){
-        if(number % 10 > maxi){
+        if(number % 10 >= maxi){
            maxi2 = maxi;
            maxi = number % 10;
         }
-        if(number % 10 >= maxi && number % 10 >= maxi2){
+        if(number % 10 < maxi && number % 10 > maxi2){
             maxi2 = number % 10;
         }
         number = number / 10;
